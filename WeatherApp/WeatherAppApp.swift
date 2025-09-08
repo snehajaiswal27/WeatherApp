b54024c8057f6dd4d04cee952035226e07a444b1
snehajaiswal27 <60898932+snehajaiswal27@users.noreply.github.com>
2025-09-07 21:34:58 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sneha Jaiswal on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Sneha Jaiswal on 9/7/25.
+//
+
+import Testing
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
